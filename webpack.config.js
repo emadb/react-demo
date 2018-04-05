756769994d6b6ff69a7b9b03478c7e565bd343b5
Emanuele DelBono <emanuele@codiceplastico.com>
2018-04-05 15:03:20 +0200

    fix bundle name

--- webpack.config.js
+++ webpack.config.js
@@ -34,20 +34,8 @@ module.exports = {
   optimization: {
 		splitChunks: {
 			cacheGroups: {
-				commons: {
-					chunks: "initial",
-					minChunks: 2,
-					maxInitialRequests: 5, 
-					minSize: 0 
-				},
-				vendor: {
-					test: /node_modules/,
-					chunks: "initial",
-					name: "vendor",
-					priority: 10,
-					enforce: true
-				}
-			}
+        commons: { test: /[\\/]node_modules[\\/]/, name: "vendors", chunks: "all" }
+      }
 		}
 	},
   module: {
